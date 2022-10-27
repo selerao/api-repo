9bff99adfb67f22c721a2e05857a0c9757de6a91
Victor Seleri <victorseleri@gmail.com>
2022-10-27 15:38:23 -0300

    Spring

---
+++ apifb/src/main/java/com/api/apifb/ApifbApplication.java
@@ -0,0 +1,13 @@
+package com.api.apifb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifbApplication.class, args);
+	}
+
+}
---
+++ apifb/src/test/java/com/api/apifb/ApifbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apifb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
