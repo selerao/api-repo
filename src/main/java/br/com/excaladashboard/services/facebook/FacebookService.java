184d4e8e5b84a316403ff2826ef89eac68469ab6
nicolas.fujisawa <nicolas.fujisawa@hotmail.com>
2022-11-23 22:48:37 -0300

    detalhamento das informações da integração

--- src/main/java/br/com/excaladashboard/services/facebook/FacebookService.java
+++ src/main/java/br/com/excaladashboard/services/facebook/FacebookService.java
@@ -3,9 +3,13 @@ package br.com.excaladashboard.services.facebook;
 import com.facebook.ads.sdk.APIException;
 import com.facebook.ads.sdk.APINodeList;
 import com.facebook.ads.sdk.AdAccount;
+import com.facebook.ads.sdk.AdsInsights;
 import com.facebook.ads.sdk.Campaign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FacebookService {
@@ -15,11 +19,35 @@ public class FacebookService {
 
     public APINodeList<Campaign> getCampaings() {
         AdAccount adAccount = this.facebookConnection.getAdAccount();
+        adAccount.getAdCreatives().requestAllFields().execute().get(0);
         try {
             return adAccount.getCampaigns().requestNameField().execute();
         } catch (APIException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public APINodeList<AdsInsights> getInsights(Campaign campaign, AdsInsights.EnumDatePreset datePreset) {
+        List<String> fields = new ArrayList<>();
+        fields.add("clicks");
+        fields.add("cpc");
+        fields.add("ctr");
+        fields.add("cpm");
+        fields.add("frequency");
+        fields.add("cost_per_action_type");
+        fields.add("actions");
+        fields.add("spend");
+
+        APINodeList<AdsInsights> adsInsights;
+        try {
+            adsInsights = campaign.getInsights()
+                    .requestFields(fields)
+                    .setDatePreset(datePreset)
+                    .execute();
+        } catch (APIException e) {
+            throw new RuntimeException(e);
+        }
+        return adsInsights;
     }
 
 }
--- src/main/java/br/com/excaladashboard/services/jobs/FacebookScheduleJob.java
+++ src/main/java/br/com/excaladashboard/services/jobs/FacebookScheduleJob.java
@@ -1,12 +1,20 @@
 package br.com.excaladashboard.services.jobs;
 
 import br.com.excaladashboard.services.facebook.FacebookService;
+import com.facebook.ads.sdk.APIException;
+import com.facebook.ads.sdk.APINodeList;
+import com.facebook.ads.sdk.AdsActionStats;
+import com.facebook.ads.sdk.AdsInsights;
+import com.facebook.ads.sdk.Campaign;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @EnableScheduling
@@ -22,8 +30,41 @@ public class FacebookScheduleJob {
     @Scheduled(cron = CRON)
     public void executeJob() {
         // aqui deve ficar o serviço que popula os dados da API do facebook
-        String campaignName = this.facebookService.getCampaings().get(0).getFieldName();
-        LOG.info("nome da campanha (ao vivo): {}", campaignName);
+        APINodeList<Campaign> campaigns = this.facebookService.getCampaings();
+        for (Campaign campaign : campaigns) {
+            LOG.info("Campanha: {}", campaign.getFieldName());
+            APINodeList<AdsInsights> insights =
+                    this.facebookService.getInsights(campaign, AdsInsights.EnumDatePreset.VALUE_MAXIMUM);
+            if (insights.size() == 1) {
+                AdsInsights insight = insights.get(0);
+                LOG.info("clicks: {}", insight.getFieldClicks());
+                LOG.info("cpc: {}", insight.getFieldCpc());
+                LOG.info("cpm: {}", insight.getFieldCpm());
+                LOG.info("frequency: {}", insight.getFieldFrequency());
+                LOG.info("spend: {}", insight.getFieldSpend());
+
+                String costPerAction = this.getValueFromAction(insight.getFieldCostPerActionType());
+                LOG.info("cost_per_action_type: {}", costPerAction);
+
+                String resultados = this.getValueFromAction(insight.getFieldActions());
+                LOG.info("resultados: {}", resultados);
+                LOG.info("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            }
+        }
+
+    }
+
+    private String getValueFromAction(List<AdsActionStats> actions) {
+        String actionValue = "";
+        for (AdsActionStats action : actions) {
+            if (Objects.equals(action.getFieldActionType(), "lead")) {
+                actionValue = action.getFieldValue();
+                break;
+            } else if (Objects.equals(action.getFieldActionType(), "link_click")) {
+                actionValue = action.getFieldValue();
+            }
+        }
+        return actionValue;
     }
 
 }
